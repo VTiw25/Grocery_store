83fe2ee69df572d4d2f6f65acba22c89a1dc6bdc
VTiw25 <vaibhavtiwari1410@gmail.com>
2024-08-31 23:18:56 +0530

    Add files via upload

--- grocery_app/backend/sql_connection.py
+++ grocery_app/backend/sql_connection.py
@@ -1,15 +1,24 @@
-import mysql.connector
 import os
+from dotenv import load_dotenv
+import mysql.connector
+from mysql.connector import Error
+
+# Load environment variables from .env file
+load_dotenv()
 
 __cnx = None
 
 def get_sql_connection():
     global __cnx
-    if __cnx is None:
+    try:
         __cnx = mysql.connector.connect(
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
-            host=os.getenv('DB_HOST'),
-            database=os.getenv('DB_NAME')
+            user=os.getenv("DB_USER"),
+            host=os.getenv("DB_HOST"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
+        if __cnx.is_connected():
+            print("Successfully connected to the database")
+    except Error as e:
+        print("Error while connecting to MySQL", e)
     return __cnx
